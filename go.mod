16be5686ce681fb548feffc247b0515fc7cf9eaf
Andrew Glaude <aglaude@vistaprint.com>
2021-05-21 10:49:11 -0400

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ajgajg1134/test-golang-fail2
+
+go 1.16
+
+require github.com/ajgajg1134/test-golang-failure-with-long-path-windows v0.0.0-20210521144334-f87fe212f613
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main_test
+
+import (
+	just_a_few_more_chars_are_needed_here "github.com/ajgajg1134/test-golang-failure-with-long-path-windows/folder-level-a/folder-level-b-much-longer-than-a/folder-level-c-with-even-more-characters-much-longer-than-b/just-a-few-more-chars-are-needed-here"
+	"testing"
+)
+
+func TestHeres_A_Test(t *testing.T) {
+	_ = just_a_few_more_chars_are_needed_here.TheBigOne{}
+}
